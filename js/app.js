18b54c984f63ada63651340d0ec6319836ca2082
boontawatk <boontawat.k@gmail.com>
2020-10-13 20:42:30 +0700

    edit page style

--- js/app.js
+++ js/app.js
@@ -1,1 +1,61 @@
-console.log("hello!");
+let Bio = {
+  name: "Boontawat",
+  surname: "Kumpiroj",
+  profilePic: "img/profile.jpg",
+  intro: "I'm a multipotentialolie who interest in cutting-edge technology.",
+  interestRoles: [
+    "Full Stack Developer",
+    "Backend Developer",
+    "Software Engineer",
+  ]
+};
+
+let contact= {
+    tel: "+6684-018-5358",
+    email: "boontawat.k@gmail.com",
+    github: "",
+    Linkedin: "",
+    Location: "Bangkok, Thailand"
+}
+
+let workEx = [
+  {
+    Location: "HiveGround",
+    Roles: "Firmware Developer Intern",
+    description: "asfsaf"
+  },
+  {
+    Location: "Mitsubishi Electric Automation Thailand",
+    Roles: "Design Engineer",
+    description: "asfsaf"
+  },
+  {
+    Location: "First Multitec",
+    Roles: "Sales Engineer",
+    description: "asfsaf"
+  }
+];
+
+let project=[
+    {
+        name:"sdaf",
+        description:"aweg",
+    },
+    {
+        name:"sdaf",
+        description:"aweg",
+    }
+]
+
+let Education=[
+    {
+        place: "Chulalongkorn University",
+        degree: "B.E.Mechanical Engineering",
+        years: "2015-2018"
+    },
+    {
+        place: "Bodindecha(Sing Singhaseni)2 School",
+        degree: "Intensive Math Science Program",
+        years: "2009-2014"
+    },
+]
