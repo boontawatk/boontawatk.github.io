744869548211ff9efa0efdb30b4ba6af99cddb1d
boontawatk <boontawat.k@gmail.com>
2020-10-16 11:05:24 +0700

    binding all Data

--- js/app.js
+++ js/app.js
@@ -11,38 +11,38 @@ var resumeRawData = {
   contact: {
     tel: "+6684-018-5358",
     email: "boontawat.k@gmail.com",
-    github: "",
-    Linkedin: "",
+    github: "aaa",
+    Linkedin: "https://www.linkedin.com/in/boontawat-kumpiroj",
     Location: "Bangkok, Thailand",
   },
   workEx: [
     {
-      Location: "HiveGround",
-      Roles: "Firmware Developer Intern",
-      description: "asfsaf",
+      company: "HiveGround",
+      role: "Firmware Developer Intern",
+      description: "HiveGround is robotic start-up company, I was an intern there for 2 months during summer. They gave me projects to develop firmware of micro-controller for UAV(Drone). I worked with 2 different brands of micro-controller Pixhawk and STM32 which introduce me to software development world. I've learn how micro-controller works in both high-level and low-level. I also learn about software development tools such as VScode, terminal, and Git Version Control",
     },
     {
-      Location: "Mitsubishi Electric Automation Thailand",
-      Roles: "Design Engineer",
-      description: "asfsaf",
+      company: "Mitsubishi Electric Automation Thailand",
+      role: "Design Engineer",
+      description: "Mitsubishi Electric Automation Thailand is market-lead motor&pump manufacturer, I worked there for 8 months. My role is Design Engineer in Pump Design team, which has responsibility to design, modify and develop pump for our clients. I worked with fluid mechanics, CAD software, multi-physics simulation software. It's not only my responsibility there, they gave me a job to build their website for the team which introduce me in web development area.",
     },
     {
-      Location: "First Multitec",
-      Roles: "Sales Engineer",
-      description: "asfsaf",
+      company: "First Multitec",
+      role: "Sales Engineer",
+      description: "First Multitec is sales representative company of machinery in cement & bulk material industry. I worked there for 5 months. My role is to corporate with clients to sell the machine that meet their requirement and contact with suppliers around the world to serve after sales services.",
     },
   ],
-  project: [
+  projects: [
     {
-      name: "sdaf",
-      description: "aweg",
+      name: "Vitae iure",
+      description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam neque eos possimus, amet minima qui? Ipsa corrupti dolor officiis optio aliquam, consequuntur itaque praesentium dignissimos veritatis nam earum consectetur eos!",
     },
     {
-      name: "sdaf",
-      description: "aweg",
+      name: "Recusandae",
+      description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam neque eos possimus, amet minima qui? Ipsa corrupti dolor officiis optio aliquam, consequuntur itaque praesentium dignissimos veritatis nam earum consectetur eos!",
     },
   ],
-  Education : [
+  Education: [
     {
       place: "Chulalongkorn University",
       degree: "B.E.Mechanical Engineering",
@@ -53,14 +53,54 @@ var resumeRawData = {
       degree: "Intensive Math Science Program",
       years: "2009-2014",
     },
-  ]
+  ],
+  onlineCourse:[
+    {
+      courseName:"data science",
+      certCredent:"",
+      description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate magnam eaque consequatur suscipit itaque adipisci doloremque explicabo nulla natus tempora accusantium, aspernatur neque officia ducimus ipsum quod alias culpa quasi?"
+    },
+    {
+      courseName:"web dev bootcamp",
+      certCredent:"",
+      description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate magnam eaque consequatur suscipit itaque adipisci doloremque explicabo nulla natus tempora accusantium, aspernatur neque officia ducimus ipsum quod alias culpa quasi?"
+    }
+  ],
+  skills:{
+    language:[
+      "HTML",
+      "CSS",
+      "Javascript",
+    ],
+    technology:[
+      "NodeJS",
+      "ExpressJS",
+      "MongoDB & MongooseJS",
+      "KnockOutJS",
+      "Bootstrap 4",
+      "Semantic UI",
+      "Wordpress (Usage)"
+    ],
+    tools:[
+      "VScode",
+      "Git version control"
+    ]
+    ,
+    softSkill:[
+      "Good Self Learner",
+      "Good team player"
+    ]
+  }
 };
 
-let resumeData=function(data){
-    let self=this;
-    self.fullname=ko.computed(function(){
-        return data.name+" "+data.surname;
-    });
-}
+let resumeData = function (data) {
+  let self = this;
+  self.fullname = ko.computed(function () {
+    return data.name + " " + data.surname;
+  });
+  for(key in data){
+    self[key]=ko.observable(data[key]);
+  }
+};
 
 ko.applyBindings(new resumeData(resumeRawData));
